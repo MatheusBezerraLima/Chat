74db46523c76e08810fe735cab0c01fab7423b0d
Matheus Bezerra Lima <matheusbeducation@gmail.com>
2024-10-19 15:35:23 -0300

    up

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
+
+wss.on("connection", (ws) =>{
+    // ws é a variavel do usuario atual
+    // mensagem de erro 
+    ws.on("error", console.error)
+
+    // função que será executada quando o usuario enviar uma mensagem para o servidor 
+    ws.on("message", (data) =>{
+        // envia a mensagem de um cliente do servidor para todos os outros
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("client connected");
+    
+})
